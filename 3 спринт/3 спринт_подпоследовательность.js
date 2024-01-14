93a1385142429461fdcdb458c4f1e23c19c8d28a
Maria Arkhipova <marias1403@yandex.ru>
2024-01-14 15:44:31 +0900

    feat: subsequence added

--- 3 спринт/3 спринт_подпоследовательность.js
+++ 3 спринт/3 спринт_подпоследовательность.js
@@ -1,32 +1,49 @@
-let readline = require('readline');
-let rl = readline.createInterface({
+const _readline = require('readline');
+
+const _reader = _readline.createInterface({
   input: process.stdin
 });
 
-let lines = [];
+const _inputLines = [];
+let _curLine = 0;
 
-function readLine (line) {
-  if (line.trim() !== '') {
-    lines.push(line);
+_reader.on('line', line => {
+  _inputLines.push(line);
+});
+
+process.stdin.on('end', solve);
+
+function subsequence(s1, s2) {
+  if (s1.length > s2.length) return false;
+
+  let p1 = 0;
+  let p2 = 0;
+
+  while (p1 < s1.length && p2 < s2.length) {
+    if (s1[p1] === s2[p2]) {
+      p1++;
+      p2++;
+    } else {
+      p2++;
+    }
+  }
+
+  return p1 === s1.length;
+}
+
+function solve() {
+  const s = readLine();
+  const t = readLine();
+  const result = subsequence(s,t);
+  if (result) {
+    process.stdout.write(`True`);
+  } else {
+    process.stdout.write(`False`);
   }
 }
 
-rl.on('line', readLine).on('close', () => {
-  let string1 = lines[0].split('');
-  let string2 = lines[1].split('');
-
-  function subsequence(s1, s2) {
-    let p1 = 0;
-    let p2 = 0;
-    while (p1 < s1.length && p2 < s2.length) {
-      if (s1[p1] === s2[p2]) {
-        p1++;
-        p2++;
-      } else {
-        p2++;
-      }
-    }
-    console.log(p1 === s1.length);
-  }
-  subsequence(string1, string2);
-});
+function readLine() {
+  const n = _inputLines[_curLine];
+  _curLine++;
+  return n;
+}
